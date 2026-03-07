a8cb2d6c4c6f4786ca37b16be84623e4af4a1041
academialibredegeomatica <pueblosvivos1@gmail.com>
2026-03-07 03:55:45 +0000

    fdsd

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,10 @@
 import { defineCollection, z } from 'astro:content';
 
 const docs = defineCollection({
-  // El schema es el "contrato" que le dice a Astro qué datos esperar
   schema: ({ image }) => z.object({
     title: z.string(),
     empresa: z.string().optional(),
-    tipo: z.string().optional(), // Cambiado de "tipo de curso" a "tipo" para que coincida con tu .md
+    tipo: z.string().optional(), // SIN ESPACIOS AQUÍ
     botonTexto: z.string().optional(),
     image: image().optional(),
     weight: z.number().optional(),
